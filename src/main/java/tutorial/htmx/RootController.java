039318820a4d8b4af75a249e6682bb26e208913b
Jumbo <sarankon.hi@gmail.com>
2023-06-19 16:12:01 +0700

    update code

--- src/main/java/tutorial/htmx/RootController.java
+++ src/main/java/tutorial/htmx/RootController.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
 @RequestMapping("/")
@@ -17,6 +20,16 @@ public class RootController {
 		var list = List.of("Albert Einstein", "Niels Bohr", "James Clerk Maxwell");
 		model.addAttribute("scientists", list);
 		return "index";
+	}
+	
+	@PostMapping(headers = "HX-Request")
+	public String htmxAddTodoItem(Model model,HttpServletResponse response) {
+		
+		List list = List.of("A");
+		model.addAttribute("item", list);
+		
+		response.setHeader("HX-Trigger", "itemAdded");
+		return "fragements :: todoItem";
 	}
 
 }
